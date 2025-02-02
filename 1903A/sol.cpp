fc308e0d1e9202934717168659da8efedd9fe334
syswraith <syswraith@proton.me>
2025-02-02 13:44:41 +0530

    py and cpp solution for like 2 problems

---
+++ 1903A/sol.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;int main(){int t;cin>>t;for(int x=0;x<t;x++){int n,k;cin>>n>>k;vector<int>array(n),array_mod(n);for(int y=0;y<n;y++)cin>>array[y];array_mod=array;sort(array_mod.begin(),array_mod.end());if(array==array_mod)cout<<"YES"<<'\n';else cout<<((k<=1)?"NO":"YES")<<'\n';}return 0;}
---
+++ 281A/sol.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include <string>
+using namespace std;int main(){ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);string input;cin>>input;input[0]=toupper(input[0]);cout<<input<<'\n';}

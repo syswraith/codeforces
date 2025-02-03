2bfb4ef074d2d1e28ba0d1b8ab7abe826491602b
syswraith <syswraith@proton.me>
2025-02-04 00:09:34 +0530

    cpp solution
    same approach as python

--- 266A/solution.cpp
+++ 266A/solution.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    
+    int n,c=0;
+    string s;
+    
+    cin >> n;
+    cin >> s;
 
-    // Begin here
-
+    for (int x = 0; x < n; x++) if (s[x] == s[x+1]) c+=1;
+    
+    cout << c << '\n';
+    
     return 0;
 }

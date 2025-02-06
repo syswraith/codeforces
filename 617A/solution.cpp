f6e22e568fa0426117221945d5c011fd843c2048
syswraith <syswraith@proton.me>
2025-02-07 02:00:18 +0530

    cpp

--- 617A/solution.cpp
+++ 617A/solution.cpp
@@ -9,7 +9,14 @@ int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 
-    // Begin here
+    int x,y,c;
+    cin >> x;
+    c = 0;
 
+    while (x>=5) x-=5,c++;
+
+    if (x!=0) c++;
+    
+    cout << c << '\n';
     return 0;
 }

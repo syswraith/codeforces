9d92c7913ef5333df3916b94d1a18d95b72c2465
syswraith <syswraith@proton.me>
2025-02-07 19:11:42 +0530

    added minify and alias

--- solve.sh
+++ solve.sh
@@ -4,10 +4,11 @@ read -p "Enter problem: " problem
 mkdir $problem
 cd $problem
 touch solution.py solution.cpp
-cat ../templates/template.cpp > solution.cpp
+cat ../config/template.cpp > solution.cpp
 contest_id=$(echo $problem | rev | cut -c 2- | rev)
 problem_index=$(echo $problem | rev | cut -c 1)
 curl -s "https://codeforces.com/api/problemset.problems" | jq --arg contest_id "$contest_id" --arg problem_index "$problem_index" '.result.problems[] | select(.contestId == ($contest_id | tonumber) and .index == $problem_index) | {name, tags}' > README.md
+alias minify="../config/minify"
 git add .
 git commit -m "Initial commit"
 git push

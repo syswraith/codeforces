5b77ada990fdc94975fd290a68243f0b5d522f08
syswraith <syswraith@proton.me>
2025-02-08 13:02:36 +0530

    json thing

--- solve.sh
+++ solve.sh
@@ -7,7 +7,9 @@ touch solution.py solution.cpp
 cat ../config/template.cpp > solution.cpp
 contest_id=$(echo $problem | rev | cut -c 2- | rev)
 problem_index=$(echo $problem | rev | cut -c 1)
-curl -s "https://codeforces.com/api/problemset.problems" | jq --arg contest_id "$contest_id" --arg problem_index "$problem_index" '.result.problems[] | select(.contestId == ($contest_id | tonumber) and .index == $problem_index) | {name, tags}' > README.md
+echo '```json' > README.md
+curl -s "https://codeforces.com/api/problemset.problems" | jq --arg contest_id "$contest_id" --arg problem_index "$problem_index" '.result.problems[] | select(.contestId == ($contest_id | tonumber) and .index == $problem_index) | {name, tags}' >> README.md
+echo '```' >> README.md
 alias minify="../config/minify"
 git add .
 git commit -m "Initial commit"

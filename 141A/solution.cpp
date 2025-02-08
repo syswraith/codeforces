a808f24707c2cffe64bb266e661ed849c1cef406
syswraith <syswraith@proton.me>
2025-02-08 12:57:44 +0530

    cpp solution
     Please enter the commit message for your changes. Lines starting

--- 141A/solution.cpp
+++ 141A/solution.cpp
@@ -9,7 +9,24 @@ int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 
-    // Begin here
+    string guest, host, pile, guesthost;
+    cin >> guest >> host >> pile;
+    guesthost = guest + host;
+    vector<char> pileVec(pile.begin(), pile.end());
+
+    for (char x : guesthost)
+    {
+	auto it = find(pileVec.begin(), pileVec.end(), x);
+	if (it != pileVec.end())
+	{
+	    pileVec.erase(it);
+	}
+	else break;
+    }
+
+    cout << ((pileVec.empty())?"YES":"NO") << '\n';
 
     return 0;
 }
+
+

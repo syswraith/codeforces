87f8d065934682330969a82465894f7e976d9d42
syswraith <syswraith@proton.me>
2025-02-10 19:48:05 +0530

    cpp solution

--- 486A/solution.cpp
+++ 486A/solution.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-
-    // Begin here
-
+    int n;
+    cin >> n;
+    cout << (floor(n/2)*(floor(n/2)+1))-pow(floor((n+1)/2), 2) << '\n';
     return 0;
 }

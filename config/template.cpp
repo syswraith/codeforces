4eb2624fb4b6514cb255f5813b580c6f511f5c55
syswraith <syswraith@proton.me>
2025-02-10 19:56:07 +0530

    template changed

--- config/template.cpp
+++ config/template.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
+#include <bits/stdc++.h>
+#define FOR(a,c) for(int a=0;a<c;a++)
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);

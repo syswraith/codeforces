64cc9f4a499224afce889ad36c5684c9f0f00ae7
syswraith <syswraith@proton.me>
2025-02-12 21:33:26 +0530

    cpp and py soln

--- 977A/solution.cpp
+++ 977A/solution.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 #define FOR(a,c) for(int a=0;a<c;a++)
+using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-
-    // Begin here
-
+    long long n,k;
+    cin >> n >> k;
+    while (k > 0) (n%10==0)?n/=10:n-=1, k-=1;
+    cout << n << '\n';
     return 0;
 }

c2bbad4b532a066ccda21ddebec40aa5b61084d7
syswraith <syswraith@proton.me>
2025-02-12 21:18:55 +0530

    Initial commit for 977A

---
+++ 977A/README.md
@@ -0,0 +1,8 @@
+```json
+{
+  "name": "Wrong Subtraction",
+  "tags": [
+    "implementation"
+  ]
+}
+```
---
+++ 977A/solution.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define FOR(a,c) for(int a=0;a<c;a++)
+int main()
+{
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    // Begin here
+
+    return 0;
+}

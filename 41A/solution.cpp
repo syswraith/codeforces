e5e0289cf222d0f37d5c3ea04fda3c2c638699fa
syswraith <syswraith@proton.me>
2025-02-16 00:01:39 +0530

    py and cpp solution

--- 41A/solution.cpp
+++ 41A/solution.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-
-    // Begin here
-
+    string s,t;
+    cin >> s >> t;
+    reverse(s.begin(),s.end());
+    cout << ((s==t)?"YES":"NO") << '\n';
     return 0;
 }
---
+++ 41A/solution.py
@@ -0,0 +1,1 @@
+s,t=input(),input();print("YES"if t==s[::-1]else"NO")

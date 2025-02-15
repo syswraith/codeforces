1016af292187d29f92ca0777ff07050e58f309e4
syswraith <syswraith@proton.me>
2025-02-15 23:48:41 +0530

    2065A

--- 2065A/solution.cpp
+++ 2065A/solution.cpp
@@ -1,15 +1,21 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-
-    // Begin here
-
+    int x, y, t;
+    cin >> t;
+    for (x = 0; x < t; x++)
+    {
+        string w;
+        cin >> w;
+        if (w == "us") cout << "si\n";
+        else 
+	{
+	    w.replace("us\n", "i\n");
+	}
+    }
     return 0;
 }
+

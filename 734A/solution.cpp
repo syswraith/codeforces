e5a0a4c55a6123bf3228c34fa37d2fe7d772ac1f
syswraith <syswraith@proton.me>
2025-02-16 16:09:01 +0530

    cpp

--- 734A/solution.cpp
+++ 734A/solution.cpp
@@ -6,7 +6,14 @@ int main()
 {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 
-    // Begin here
+    int n, o, A = 0;
+    string s;
+    cin >> n >> s;
 
+    for (o = 0; o < n; o++) if (s[o] == 'A') A++;
+    if (A > (n-A)) cout << "Anton";
+    else if (A < (n-A)) cout << "Danik";
+    else cout << "Friendship";
+    cout << '\n';
     return 0;
 }

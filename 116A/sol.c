79dbb0faf252ff926229fe6df338cf804035db95
syswraith <syswraith@proton.me>
2025-02-17 23:11:01 +0530

    c and cpp solutions

---
+++ 116A/sol.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int main(){int counter_1, counter_2, no_of_inputs, passengers, max_passengers;passengers=max_passengers=0;scanf("%d",&no_of_inputs);for(counter_1=0;counter_1<no_of_inputs;counter_1++){int a, b;scanf("%d%d",&a,&b);passengers+=b-a;int max_capacity;max_passengers=((max_passengers>passengers)?max_passengers:passengers);}printf("%d\n",max_passengers);return 0;}
--- 116A/solution.c
+++ 116A/solution.c
@@ -1,23 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 
     int counter_1, counter_2, no_of_inputs, passengers, max_passengers;
     passengers=max_passengers=0;
-    cin >> no_of_inputs;
-
+    scanf("%d", &no_of_inputs);
     for (counter_1 = 0; counter_1 < no_of_inputs; counter_1++)
     {
 	int a, b;
-	cin >> a >> b;
+	scanf("%d%d", &a, &b);
 	passengers += b-a;
 	int max_capacity;
 	max_passengers = ((max_passengers > passengers)?max_passengers:passengers);
     }
-    cout << max_passengers << '\n';
-
+    printf("%d\n",max_passengers);
     return 0;
 }

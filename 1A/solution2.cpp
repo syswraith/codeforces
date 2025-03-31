17d32e3d71594ef795dbab013256b44882529929
syswraith <syswraith@proton.me>
2025-03-31 13:02:49 +0530

    Mon Mar 31 12:53:23 PM IST 2025

--- 1A/solution2.cpp
+++ 1A/solution2.cpp
@@ -1,14 +1,11 @@
-// integer division trick as suggested by chatgpt
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    long long n,m,a;
-    cin >> n >> m >> a;
-    cout << (n+a-1)/a * (m+a-1)/a << '\n';
+    long long n, m, a;
+    std::cin >> n >> m >> a;
+    std::cout << ((n + a - 1) / a) * ((m + a - 1) / a) << std::endl;
     return 0;
 }
